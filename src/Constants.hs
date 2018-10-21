3909eeded897b4678e00f3f1ef7ead4112d7b1eb
Egor Dmitriev <egordmitriev2@gmail.com>
2018-10-21 15:48:06 +0200

    Added sprite loading

--- src/Constants.hs
+++ src/Constants.hs
@@ -1,8 +1,14 @@
 module Constants where
 
+import Graphics.Gloss.Data.Color(black)
+
+-- | Window name
+gameName = "FP Pacman"
+background = black
+
 -- | Windows size constants
 width, height, offset :: Int
-width = 248
+width = 372
 height = 496
 offset = 100
 
@@ -11,4 +17,11 @@ fps :: Int
 fps = 60
 
 -- | Paths
-resourceDir = "../res/"
+resourceDir = "res/"
+
+spriteScale :: Float
+spriteScale = 2
+
+-- | Sprite Interval
+spriteInterval :: Float
+spriteInterval = 1
---
+++ src/Engine/Graphics/Loading.hs
@@ -0,0 +1,34 @@
+module Engine.Graphics.Loading (
+    loadStaticSprite,
+    loadAnimatedSprite
+) where
+
+import Text.Printf
+import Graphics.Gloss.Game
+import Graphics.Gloss (Picture(..))
+import Engine.Graphics.Models
+import Constants
+
+-- | Loads creates filenames for given resource by identifier and amount of sprites
+spriteFileNames :: String -> Int -> [String]
+spriteFileNames identifier n = (\i -> resourceDir ++ identifier ++ "/" ++ (printf "%02d" i) ++ ".png") <$> [0.. n]
+
+-- | Loads an image from file. If failed, then throws errors
+loadImage :: String -> Picture
+loadImage file = case img of Bitmap _ _ _ _ -> Scale spriteScale spriteScale img
+                             _              -> error ("Cannot load " ++ file)
+                 where img = png file
+
+
+-- | Loads sprites for all given images. Errors if none is found
+loadImages :: [String] -> [Picture]
+loadImages = map loadImage
+
+-- | Load Sprites
+loadStaticSprite :: String -> Sprite
+loadStaticSprite identifier = createStaticSprite frame
+                              where frame = head (loadImages (spriteFileNames identifier 1))
+
+loadAnimatedSprite :: String -> Int -> AnimationType -> Float -> Sprite
+loadAnimatedSprite identifier n animType interval = createAnimatedSprite animType frames interval
+                                                    where frames = loadImages (spriteFileNames identifier n)
