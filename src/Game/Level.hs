df70890db47a1ca9a601d01d8f9af52249dc8ede
Egor Dmitriev <egordmitriev2@gmail.com>
2018-10-25 00:16:49 +0200

    Made level loading working

--- src/Game/Level.hs
+++ src/Game/Level.hs
@@ -20,19 +20,18 @@ import Engine.Graphics.Sprite
 import Engine.Graphics.Rendering
 import Resources
 import Constants
-import Debug.Trace
 
 
 {- Data structures -}
 data Powerup = PacDot | PowerPill | Cherry deriving (Show, Eq)
 
-data Tile = TileEmpty | TilePowerup Powerup | TileWall Sprite deriving (Show, Eq)
+data Tile = TileEmpty | TilePowerup Powerup | TileWall Sprite | TileDoor deriving (Show, Eq)
 
 data IsWallTile = Wall Tile | NotWall Tile deriving (Show, Eq)
 
 data Table a = Table (Vec.Vector (Vec.Vector a)) Int Int deriving (Show, Eq)
 
-data Level = Level (Table Tile) deriving (Show, Eq)
+data Level = Level {tiles :: Table Tile} deriving (Show, Eq)
 
 data TileMatcher = TileMatcher {
                       none, up, down, left, right, upLeft, upRight, downLeft, downRight :: IsWallTile
@@ -67,6 +66,7 @@ tileToSprite tile = case tile of TileEmpty               -> createEmptySprite
                                  (TilePowerup PowerPill) -> spritePowerupPowerPellet
                                  (TilePowerup Cherry)    -> spritePowerupCherry
                                  (TileWall sprite)       -> sprite
+                                 (TileDoor)              -> createEmptySprite
 
 createTileMatcher :: Pos -> Level -> TileMatcher
 createTileMatcher p (Level t) = TileMatcher{
@@ -85,11 +85,51 @@ isWallTile :: Tile -> IsWallTile
 isWallTile t@(TileWall a) = Wall t
 isWallTile t = NotWall t
 
-matchWallSprite :: Pos -> TileMatcher -> Tile
-matchWallSprite p TileMatcher{none=Wall _, left = Wall _, right = NotWall _, up = NotWall _, down = Wall _, upRight = NotWall _, downLeft = NotWall _} = TileWall spriteTileSCornerLD
-matchWallSprite _ TileMatcher{none=Wall _, left = NotWall _, right = Wall _, up = NotWall _, down = Wall _, upLeft = NotWall _, downRight = NotWall _ } = TileWall spriteTileSCornerRD
-matchWallSprite _ TileMatcher{none=Wall r} = r
-matchWallSprite _ TileMatcher{none=NotWall r} = r
+matchWallSprite :: TileMatcher -> Tile
+matchWallSprite TileMatcher{none=Wall _, left = Wall lw, right = NotWall _, up = Wall uw, down = NotWall _}
+    | lw == TileWall spriteTileSStraightD && uw == TileWall spriteTileSStraightR = TileWall spriteTileSCornerLU
+    | otherwise = TileWall spriteTileFCornerLUUL
+matchWallSprite TileMatcher{none=Wall _, left = NotWall _, right = Wall rw, up = Wall uw, down = NotWall _}
+    | rw == TileWall spriteTileSStraightD && uw == TileWall spriteTileSStraightL = TileWall spriteTileSCornerRU
+    | otherwise = TileWall spriteTileFCornerRUUR
+matchWallSprite TileMatcher{none=Wall _, left = Wall lw, right = NotWall _, up = NotWall _, down = Wall dw}
+    | lw == TileWall spriteTileSStraightU && dw == TileWall spriteTileSStraightR = TileWall spriteTileSCornerLD
+    | otherwise = TileWall spriteTileFCornerLDDL
+matchWallSprite TileMatcher{none=Wall _, left = NotWall _, right = Wall rw, up = NotWall _, down = Wall dw}
+    | rw == TileWall spriteTileSStraightU && dw == TileWall spriteTileSStraightL = TileWall spriteTileSCornerRD
+    | otherwise = TileWall spriteTileFCornerRDDR
+
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = NotWall _, up = NotWall _, down = Wall _, upRight = NotWall _, downLeft = NotWall _} = TileWall spriteTileSCornerLD
+matchWallSprite TileMatcher{none=Wall _, left = NotWall _, right = Wall _, up = NotWall _, down = Wall _, upLeft = NotWall _, downRight = NotWall _ } = TileWall spriteTileSCornerRD
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = NotWall _, up = Wall _, down = NotWall _, upLeft = NotWall _, downRight = NotWall _ } = TileWall spriteTileSCornerLU
+matchWallSprite TileMatcher{none=Wall _, left = NotWall _, right = Wall _, up = Wall _, down = NotWall _, upRight = NotWall _, downRight = NotWall _ } = TileWall spriteTileSCornerRU
+
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = NotWall _, up = Wall _, down = Wall _, upRight = NotWall _, upLeft = NotWall _} = TileWall spriteTileSSplitUDL
+matchWallSprite TileMatcher{none=Wall _, left = NotWall _, right = Wall _, up = Wall _, down = Wall _, upRight = NotWall _, upLeft = NotWall _} = TileWall spriteTileSSplitUDR
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = NotWall _, up = Wall _, down = Wall _, downRight = NotWall _, downLeft = NotWall _} = TileWall spriteTileSSplitDUL
+matchWallSprite TileMatcher{none=Wall _, left = NotWall _, right = Wall _, up = Wall _, down = Wall _, downRight = NotWall _, downLeft = NotWall _} = TileWall spriteTileSSplitDUR
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = Wall _, up = NotWall _, down = Wall _, downRight = Wall _, downLeft = NotWall _} = TileWall spriteTileSSplitLRD
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = Wall _, up = NotWall _, down = Wall _, downRight = NotWall _, downLeft = Wall _} = TileWall spriteTileSSplitRLD
+
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, upLeft = NotWall _, up = Wall _} = TileWall spriteTileFCornerLUUL
+matchWallSprite TileMatcher{none=Wall _, right = Wall _, upRight = NotWall _, up = Wall _} = TileWall spriteTileFCornerRUUR
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, downLeft = NotWall _, down = Wall _} = TileWall spriteTileFCornerLDDL
+matchWallSprite TileMatcher{none=Wall _, right = Wall _, downRight = NotWall _, down = Wall _} = TileWall spriteTileFCornerRDDR
+
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = Wall _, up = NotWall _, down = Wall _} = TileWall spriteTileFStraightD
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = Wall _, up = NotWall _, down = Wall _} = TileWall spriteTileFStraightD
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = Wall _, up = Wall _, down = NotWall _} = TileWall spriteTileFStraightU
+matchWallSprite TileMatcher{none=Wall _, left = Wall _, right = NotWall _, up = Wall _, down = Wall _} = TileWall spriteTileFStraightL
+matchWallSprite TileMatcher{none=Wall _, left = NotWall _, right = Wall _, up = Wall _, down = Wall _} = TileWall spriteTileFStraightR
+
+matchWallSprite TileMatcher{none=Wall _, left = Wall lw, right = NotWall TileEmpty, up = NotWall _, down = NotWall _, upLeft = NotWall _, downLeft = NotWall _} = lw
+matchWallSprite TileMatcher{none=Wall _, left = NotWall TileEmpty, right = Wall rw, up = NotWall _, down = NotWall _, upRight = NotWall _, downRight = NotWall _} = rw
+
+matchWallSprite TileMatcher{none=Wall _, left = Wall lw, right = NotWall TileDoor, up = NotWall _, down = NotWall _, upLeft = NotWall _, downLeft = NotWall _} = TileWall spriteTileCEndR
+matchWallSprite TileMatcher{none=Wall _, left = NotWall TileDoor, right = Wall rw, up = NotWall _, down = NotWall _, upRight = NotWall _, downRight = NotWall _} = TileWall spriteTileCEndL
+
+matchWallSprite TileMatcher{none=Wall r} = r
+matchWallSprite TileMatcher{none=NotWall r} = r
 
 -- Split this into level loading
 -- | Reads raw level into a character map
@@ -102,6 +142,11 @@ parseTile :: Char -> Tile
 parseTile char = case char of '#' -> TileWall createEmptySprite
                               '.' -> TilePowerup PacDot
                               '@' -> TilePowerup PowerPill
+                              '_' -> TileDoor
+                              'u' -> TileWall spriteTileSStraightU
+                              'd' -> TileWall spriteTileSStraightD
+                              'l' -> TileWall spriteTileSStraightL
+                              'r' -> TileWall spriteTileSStraightR
                               _   -> TileEmpty
 
 -- | Creates table from given list of lists of a type
@@ -121,9 +166,15 @@ updateLevelWalls :: Level -> Level
 updateLevelWalls l@(Level (Table vec w h)) = Level (Table nvec w h)
                                              where nvec = Vec.fromList [nrow y | y <- [0.. h-1]]
                                                    nrow y = Vec.fromList [ntile (Pos x y) | x <- [0.. w-1]]
-                                                   ntile p = matchWallSprite p (createTileMatcher p l)
+                                                   ntile p = matchWallSprite (createTileMatcher p l)
+
+
 
 -- | Reads level from a file. Warning IO
 readLevel :: String -> IO Level
 readLevel file = do rawLevel <- readRawLevel file
-                    return (updateLevelWalls (parseLevel rawLevel))
+                    return (updateLevelWalls (updateLevelWalls (parseLevel rawLevel)))
+
+
+test (Wall (TileWall spriteTileFStraightU)) = "hi"
+test _ = "hello"
